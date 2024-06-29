0034a6a0128dc911bbd9625940f95947f94a1706
litaldigabriel <litaldigabriel@gmail.com>
2024-06-29 09:54:04 -0300

    Add files via upload

---
+++ src/componentes/Logo/index.js
@@ -0,0 +1,22 @@
+import logo from '../../imagens/logo.svg'
+import styled from 'styled-components'
+
+const LogoContainer = styled.div`
+    display: flex;
+    font-size: 30px;
+`
+const LogoImg = styled.img`
+    margin-right: 10px;
+    margin-left: 10px;
+`
+
+function Logo() {
+    return (
+        <LogoContainer>
+            <LogoImg src={logo} alt='Logo Alura Books' className='logo-img'/>
+            <p><strong>Alura</strong>Books</p>
+        </LogoContainer> 
+    )
+}
+
+export default Logo
---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,8 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    {nome: 'Liderança em Design', id: 1, src: livro},
+    {nome: 'JavaScript Assertivo', id: 2, src: livro},
+    {nome: 'Cangaceiro em JavaScript', id: 3, src: livro},
+    {nome: 'React.js', id: 4, src: livro}
+]
